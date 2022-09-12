cd51c7ee83372617ec87c2895e55b954cc36aaab
Smart732 <84396707+Smart732@users.noreply.github.com>
2022-09-12 16:30:18 +0530

    yntfhgyuj

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@
 function App() {
   return (
     <><h2>Bikash singhaniya</h2>
-    <h6>I am web devloper</h6>
+    <h6>I am singhaniya </h6>
     <a href="https://github.com/smart732/react">my source code</a></>
   );
 }
